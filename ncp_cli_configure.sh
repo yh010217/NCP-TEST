812e349052bff53d0f11fc649ecb84ab2aad87c5
choi <cyh010217@gmail.com>
2024-12-08 16:22:11 +0900

    일단 한번 해보자

---
+++ ncp_cli_configure.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/expect -f
+
+set timeout -1
+
+# Ncloud 설정
+spawn ./ncloud configure
+expect "Ncloud Access Key Id [] :"
+send "${{ secrets.CLI_CONFIGURE_ACCESS_KEY }}\r"
+expect "Ncloud Secret Access Key [] :"
+send "${{ secrets.CLI_CONFIGURE_SECRET_KEY }}\r"
+expect "Ncloud API URL (default:https://ncloud.apigw.ntruss.com) []:"
+send "\r"
+
+expect eof
