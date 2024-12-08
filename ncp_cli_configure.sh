460e54dd5f652e69fa018766adec7855c6dbf7c1
choi <cyh010217@gmail.com>
2024-12-08 16:34:39 +0900

    권한 및 경로 변경

--- ncp_cli_configure.sh
+++ ncp_cli_configure.sh
@@ -5,9 +5,9 @@ set timeout -1
 # Ncloud 설정
 spawn ./ncp_cli/cli/cli_linux/ncloud configure
 expect "Ncloud Access Key Id [] :"
-send "${{ secrets.CLI_CONFIGURE_ACCESS_KEY }}\r"
+send "test access\r"
 expect "Ncloud Secret Access Key [] :"
-send "${{ secrets.CLI_CONFIGURE_SECRET_KEY }}\r"
+send "test secret\r"
 expect "Ncloud API URL (default:https://ncloud.apigw.ntruss.com) []:"
 send "\r"
 
